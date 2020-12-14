30119b0d40c944d21a1c49561f589c1de447cec6
Karl Mulligan <klmulligan17@gmail.com>
2020-12-14 18:56:07 -0500

    added tokenization

--- load_data.py
+++ load_data.py
@@ -9,6 +9,8 @@ from torchtext import data
 from torchtext import datasets
 from torchtext.vocab import Vectors, GloVe
 import ipdb
+#from nltk.tokenize import word_tokenize
+import re
 
 def load_dataset(test_sen=None):
 
@@ -28,18 +30,34 @@ def load_dataset(test_sen=None):
     """
     
     # TODO: examine tokenization outputs
+    train_file = "train.csv"
+    val_file = "val.csv"
+    test_file = "test.csv"
+
+    def twitter_tokenize(x):
+        # stolen from https://towardsdatascience.com/use-torchtext-to-load-nlp-datasets-part-i-5da6f1c89d84
+        x = re.sub(
+                        r"[\*\"“”\n\\…\+\-\/\=\(\)‘•:\[\]\|’\!;]", " ", 
+                            str(x))
+        x = re.sub(r"[ ]+", " ", x)
+        x = re.sub(r"\!+", " !", x)
+        x = re.sub(r"\,+", " ,", x)
+        x = re.sub(r"\?+", " ?", x)
+        x = re.sub(r"\.", " .", x)
+        return x.split()
 
     tokenize = lambda x: x.split()
-    TEXT = data.Field(sequential=True, tokenize=tokenize, lower=True, include_lengths=True, batch_first=True) #fix_length=50)
+    TEXT = data.Field(sequential=True, tokenize=twitter_tokenize, lower=True, include_lengths=True, batch_first=True) #fix_length=50)
     LABEL = data.LabelField()
     #LABEL = data.LabelField(tensor_type=torch.FloatTensor)
     #train_data, test_data = datasets.IMDB.splits(TEXT, LABEL)
-    ds = data.TabularDataset(
-        path='data/labeled_data.csv', format='csv',
+    train_data, valid_data, test_data = data.TabularDataset.splits(
+        path='data/', format='csv',
+        train=train_file, validation=val_file, test=test_file,
         fields={'tweet': ('text', TEXT),
                 'class': ('label', LABEL)},
         skip_header=False)
-    train_data, test_data = ds.split(split_ratio=0.9)
+    #train_data, test_data = ds.split(split_ratio=0.9)
     #ipdb.set_trace()
     TEXT.build_vocab(train_data, vectors=GloVe(name='6B', dim=300))
     LABEL.build_vocab(train_data)
@@ -48,8 +66,10 @@ def load_dataset(test_sen=None):
     print ("Length of Text Vocabulary: " + str(len(TEXT.vocab)))
     print ("Vector size of Text Vocabulary: ", TEXT.vocab.vectors.size())
     print ("Label Length: " + str(len(LABEL.vocab)))
+    #ipdb.set_trace()
+    #print ("Example: ", train_data[234].TEXT)
 
-    train_data, valid_data = train_data.split(split_ratio=0.8888888889) # Further splitting of training_data to create new training_data & validation_data
+    #train_data, valid_data = train_data.split(split_ratio=0.8888888889) # Further splitting of training_data to create new training_data & validation_data
     train_iter, valid_iter, test_iter = data.BucketIterator.splits((train_data, valid_data, test_data), batch_size=32, sort_key=lambda x: len(x.text), repeat=False, shuffle=True)
 
     #ipdb.set_trace()
